55cc28d6c196dfb77b37aee7ab238b1120b7c2e4
Rares2504 <163425868+Rares2504@users.noreply.github.com>
2024-11-12 17:26:25 +0200

    Add files via upload

---
+++ proiect/scrpt.js
@@ -0,0 +1,9 @@
+// JavaScript pentru adăugarea unor funcționalități interactive
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Bun venit la aplicația de cumpărare bilete la cinema!");
+});
+
+// Exemplu de funcție pentru buton (dacă adaugi unul pe pagină)
+function cumparaBilet() {
+    alert("Funcționalitatea de cumpărare a biletelor va fi disponibilă în curând!");
+}
